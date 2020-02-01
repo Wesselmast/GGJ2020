e4ed6b9e81738dacf1f5fd1a6905a40c8540a544
LAPTOP-GQGBUH1P\mick <mick@gerritsen-online.nl>
2020-02-01 04:49:50 +0100

    ragdoll

---
+++ Assets/Scripts/RagdollPositionSnap.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollPositionSnap : MonoBehaviour
+{
+
+    public Transform snapPosition;
+    public Transform targetTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        targetTransform.position = snapPosition.position;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        Transform newTargetTransform = collision.transform.root.GetChild(0);
+        if (newTargetTransform.tag == "Patient") {
+            //newTargetTransform.GetComponent<Rigidbody>().isKinematic = true;
+            if (targetTransform == null) {
+                targetTransform = newTargetTransform;
+                targetTransform.position = snapPosition.position;
+                targetTransform.eulerAngles = new Vector3(90, 180, 90);
+            } else if (newTargetTransform != targetTransform) {
+                targetTransform = newTargetTransform;
+                targetTransform.position = snapPosition.position;
+            } else {
+                return;
+            }
+            Debug.Log("patient");
+        }
+    }
+}
